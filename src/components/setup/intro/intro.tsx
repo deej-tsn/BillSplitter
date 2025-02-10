f5e37f6af4ddca62f8fc113488f75d5e4583f5e8
Dempsey Palacio Tascon <dempsey.2001@hotmail.co.uk>
2025-02-10 22:30:54 +0000

    added buttons to intro, need to add functionality

--- src/components/setup/intro/intro.tsx
+++ src/components/setup/intro/intro.tsx
@@ -1,9 +1,13 @@
 export default function Intro(){
     return (
-        <div>
+        <div id="intro">
             <h1>Welcome To Bill Splitter</h1>
             <p>This tool will make dividing bills amongst friends super easy!</p>
             <p>Lets get started!</p>
+            <div>
+                <button>Generate From Photo</button>
+                <button>Fill out yourself</button>
+            </div>
         </div>
     )
 }
